20123db865751324e0d7a6020f8c830579b60dd2
kumsuzM <mkumsuz1@gmail.com>
2024-12-03 18:49:25 +1100

    Day 2 part 2

--- internal/solutions/2024/day02/solution.go
+++ internal/solutions/2024/day02/solution.go
@@ -25,7 +25,7 @@ func (s *Solver) Part1(input string) (string, error) {
 }
 
 func isSafe(report []int) bool {
-	var isAsc bool = false
+	var isAsc bool = report[1] > report[0]
 
 	for i := range report {
 		// skip past first item
@@ -33,21 +33,59 @@ func isSafe(report []int) bool {
 			continue
 		}
 		diff := math.Abs(float64(report[i]) - float64(report[i-1]))
-		if diff < 1 || diff > 3 {
-			return false
-		}
 		currIsAsc := report[i] > report[i-1]
-		if i != 1 && isAsc != currIsAsc {
+		if (diff < 1 || diff > 3) || (i > 1 && isAsc != currIsAsc) {
 			return false
 		}
-		isAsc = currIsAsc
 	}
 
 	return true
 }
 
 func (s *Solver) Part2(input string) (string, error) {
-	return "day 2 part 1 sol", nil
+	reports := parseInput(input)
+	numSafe := 0
+	for _, report := range reports {
+		if isSafe2(report) {
+			numSafe++
+		}
+	}
+
+	return strconv.Itoa(numSafe), nil
+}
+
+func isSafe2(report []int) bool {
+	if isSafe(report) {
+		return true
+	}
+
+	for i := range report {
+		dampened := make([]int, 0, len(report)-1)
+		dampened = append(dampened, report[:i]...)
+		dampened = append(dampened, report[i+1:]...)
+
+		// Check if this dampened version is safe
+		if isSafe(dampened) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func dampenable(currIsAsc bool, currIdx, before, after int) (bool, bool) {
+	// 1. first check if removing me will fix diff
+	diff := math.Abs(float64(before) - float64(after))
+	if diff < 1 || diff > 3 {
+		return false, false
+	}
+
+	// 2. second check if removing me will change isAsc
+	if currIdx == 1 {
+		return true, after > before
+	} else {
+		return currIsAsc == (after > before), currIsAsc
+	}
 }
 
 func parseInput(input string) [][]int {
